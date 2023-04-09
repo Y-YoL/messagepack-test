0c82b71033620c0d414f09f66cd010112cdec047
y-sasaki <11359025+Y-YoL@users.noreply.github.com>
2023-04-09 17:47:27 +0900

    add test

---
+++ Unity/Assets/Tests/CodeGenTest01.cs
@@ -0,0 +1,55 @@
+using System;
+using MessagePack;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Test
+{
+    using Random = UnityEngine.Random;
+
+    public class CodeGenTest01
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            MessagePackSerializer.DefaultOptions = MessagePackSerializerOptions.Standard
+                .WithResolver(GeneratedMessagePackResolver.Instance);
+        }
+
+        [Test]
+        public void ErrorWithoutResolver()
+        {
+            var x = new Data01
+            {
+                Value = Random.Range(1, int.MaxValue),
+            };
+
+            // without generated resolver
+            Assert.Throws<MessagePackSerializationException>(() => MessagePackSerializer.Serialize(x, MessagePackSerializerOptions.Standard));
+        }
+
+        [Test]
+        public void Test01()
+        {
+            var value = Random.Range(1, int.MaxValue);
+
+            var x = new Data01
+            {
+                Value = value,
+            };
+
+            var bytes = MessagePackSerializer.Serialize(x);
+
+            var y = MessagePackSerializer.Deserialize<Data01>(bytes);
+
+            Assert.AreEqual(value, y.Value);
+        }
+
+        [MessagePackObject]
+        public class Data01
+        {
+            [Key(0)]
+            public int Value { get; set; }
+        }
+    }
+}
